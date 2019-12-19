d1203c350010f5444a716947b5174335bee773c3
LorenzoTaroni <42691490+LorenzoTaroni@users.noreply.github.com>
2019-12-19 03:49:47 +0100

    Add files via upload

--- Assign02_git/prfx_script.sh
+++ Assign02_git/prfx_script.sh
@@ -6,7 +6,7 @@ echo -n > prfx_time.txt
 
 
 
-gcc -lrt -std=c99 -o prfx_sum.x prfx_sum.c
+gcc -lrt -std=c99 -fopenmp -o prfx_sum.x prfx_sum.c
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ; do
 
