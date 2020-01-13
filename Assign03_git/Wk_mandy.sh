976d93eabdaaace1aff19dd3997a826cea089092
LorenzoTaroni <42691490+LorenzoTaroni@users.noreply.github.com>
2020-01-13 19:07:26 +0100

    Add files via upload

--- Assign03_git/Wk_mandy.sh
+++ Assign03_git/Wk_mandy.sh
@@ -32,7 +32,7 @@ for threads in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ; do
 
   export OMP_NUM_THREADS=${threads}
 
-  new=$(("${threads}"*"${ny}"))
+  new=$((${threads}*${ny}))
 
   input=("${nx}" "${new}" "${xl}" "${yl}" "${xr}" "${yr}" "${imax}")
 
